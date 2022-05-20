60083f824161b75b1c7b72a1e32bc7629df56f69
Hakan Akkurt <Hakan_Akkurt@hotmail.de>
2022-05-20 22:44:36 +0200

    API Call Test

--- client/lib/homepage.dart
+++ client/lib/homepage.dart
@@ -1,11 +1,4 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-
-import 'dart:async';
-
-import 'package:http/http.dart' as http;
-
 import 'MyCard.dart';
 
 // ignore_for_file: prefer_const_constructors
@@ -37,21 +30,5 @@ class HomePage extends StatelessWidget {
         ],
       ),
     );
-  }
-
-  Future getData() async {
-    final response = await http.get(Uri.parse(
-        'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=51.450832%2C7.013056&radius=1500&type=gym&key=YOUR_API_KEY'));
-
-    if (response.statusCode == 200) {
-      // If the server did return a 200 OK response,
-      // then parse the JSON.
-      print(' !!! Place data is here !!!');
-      return jsonDecode(response.body);
-    } else {
-      // If the server did not return a 200 OK response,
-      // then throw an exception.
-      throw Exception('Failed to load place data');
-    }
   }
 }
