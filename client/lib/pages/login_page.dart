ae7a5530000edfc31ade369fa1fa7f1fea056eff
Hakan Akkurt <Hakan_Akkurt@hotmail.de>
2022-06-05 00:51:43 +0200

    Login Page Template

--- client/lib/pages/login_page.dart
+++ client/lib/pages/login_page.dart
@@ -1,14 +1,13 @@
-import 'package:client/futurebuilders/post_futurebuilder.dart';
-import 'package:client/templates/post_template.dart';
-import 'package:client/shared/mydrawer.dart';
+// ignore_for_file: deprecated_member_use
+
+import 'package:client/pages/event_page.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 
 // ignore_for_file: prefer_const_constructors
 // ignore_for_file: prefer_const_literals_to_create_immutables
 
 class LoginPage extends StatefulWidget {
-  LoginPage({Key? key}) : super(key: key);
+  const LoginPage({Key? key}) : super(key: key);
 
   @override
   State<LoginPage> createState() => _LoginPageState();
@@ -17,8 +16,112 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Login Page"),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text("Login Page"),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 60.0),
+              child: Center(
+                child: Container(
+                    width: 250,
+                    height: 250,
+                    child: Image.asset('assets/dance.jpg')),
+              ),
+            ),
+            SizedBox(
+                width: 250,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 15),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'E-Mail or Phone Number',
+                        hintText: 'Enter valid E-Mail or Phone Number'),
+                  ),
+                )),
+            SizedBox(
+                width: 250,
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                      left: 15.0, right: 15.0, top: 15, bottom: 0),
+                  child: TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Password',
+                        hintText: 'Enter secure password'),
+                  ),
+                )),
+            FlatButton(
+              onPressed: () {
+                // Forgot Password logic
+                showAlertDialog(
+                    context, "Alert Message", "Forgot Password clicked!");
+              },
+              child: Text(
+                'Forgot Password',
+                style: TextStyle(color: Colors.blue, fontSize: 15),
+              ),
+            ),
+            Container(
+              height: 50,
+              width: 250,
+              decoration: BoxDecoration(
+                  color: Colors.blue, borderRadius: BorderRadius.circular(20)),
+              child: FlatButton(
+                onPressed: () {
+                  // Login logic here
+                  Navigator.push(
+                      context, MaterialPageRoute(builder: (_) => EventPage()));
+                },
+                child: Text(
+                  'Login',
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 130,
+            ),
+            FlatButton(
+              onPressed: () {
+                // Forgot Password logic here
+                showAlertDialog(
+                    context, "Alert Message", "Create Account clicked!");
+              },
+              child: Text(
+                'Create Account',
+                style: TextStyle(color: Colors.blue, fontSize: 15),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
+}
+
+showAlertDialog(context, String title, String message) {
+  Widget okButton = TextButton(
+      onPressed: () => Navigator.pop(context, 'OK'), child: const Text('OK'));
+
+  AlertDialog alert = AlertDialog(
+    title: Text(title),
+    content: Text(message),
+    actions: [
+      okButton,
+    ],
+  );
+
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return alert;
+    },
+  );
 }
